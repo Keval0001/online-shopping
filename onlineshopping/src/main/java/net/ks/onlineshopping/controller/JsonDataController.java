c60cb1f6f69d29ce527db8d1f4cf1ef4ae81e093
Keval Shah <keval0001@gmail.com>
2017-12-07 10:10:10 -0700

    before log4j

---
+++ onlineshopping/src/main/java/net/ks/onlineshopping/controller/JsonDataController.java
@@ -0,0 +1,35 @@
+package net.ks.onlineshopping.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import net.ks.shoppingbackend.dao.ProductDAO;
+import net.ks.shoppingbackend.dto.Product;
+
+@Controller
+@RequestMapping("/json/data")
+public class JsonDataController {
+
+	@Autowired
+	private ProductDAO productDAO;
+
+	@RequestMapping("/all/products")
+	@ResponseBody // return object in form of json , convert automatically
+	public List<Product> getAllProduct() {
+
+		return productDAO.listActiveProducts();
+	}
+
+	@RequestMapping("/category/{id}/products")
+	@ResponseBody
+	public List<Product> getProductByCategory(@PathVariable int id) {
+
+		return productDAO.listActiveProductByCategory(id);
+	}
+
+}
---
+++ shoppingbackend/src/main/java/net/ks/shoppingbackend/config/HibernateConfig.java
@@ -0,0 +1,71 @@
+package net.ks.shoppingbackend.config;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBuilder;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages={"net.ks.shoppingbackend.dto"})
+@EnableTransactionManagement
+public class HibernateConfig {
+			
+	//Change the below based on the DBMS you choose
+	private final static String DATABASE_URL ="jdbc:h2:tcp://localhost/~/onlineshopping";
+	private final static String DATABASE_DRIVER ="org.h2.Driver";
+	private final static String DATABASE_DIALECT ="org.hibernate.dialect.H2Dialect";
+	private final static String DATABASE_USERNAME ="sa";
+	private final static String DATABASE_PASSWORD ="";
+	
+	// dataSource bean will be available
+	@Bean
+	public DataSource getDataSource() {
+		
+		BasicDataSource dataSource = new BasicDataSource();
+		
+		// Providing the database connection information
+		dataSource.setDriverClassName(DATABASE_DRIVER);
+		dataSource.setUrl(DATABASE_URL);
+		dataSource.setUsername(DATABASE_USERNAME);
+		dataSource.setPassword(DATABASE_PASSWORD);
+		
+		return dataSource;
+		
+	}
+	
+	// sessionFactory bean will be available, This bean will used dataSouces bean which we have created before
+	@Bean
+	public SessionFactory getSessionFactory(DataSource dataSource){
+		LocalSessionFactoryBuilder builder = new LocalSessionFactoryBuilder(dataSource);
+		builder.addProperties(getHibernateProperties());
+		//scan all entities
+		builder.scanPackages("net.ks.shoppingbackend.dto");
+		return builder.buildSessionFactory();
+	}
+
+	
+	 // All the hibernate properties will be returned in this method
+	private Properties getHibernateProperties() {
+			Properties properties = new Properties();
+			properties.put("hibernate.dialect",DATABASE_DIALECT);
+			properties.put("hibernate.show_sql","true");
+			properties.put("hibernate.format_sql","true");
+		return properties;
+	}
+	
+	// TransactionManager bean
+	@Bean
+	public HibernateTransactionManager getTransactionmanager(SessionFactory sessionFactory){
+		HibernateTransactionManager transactionManager = new  HibernateTransactionManager(sessionFactory);
+		return transactionManager;
+	}
+	
+}
