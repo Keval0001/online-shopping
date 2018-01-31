f257f35a6b54a21ee3479af22e4f9f8773a23a82
Keval Shah <keval0001@gmail.com>
2018-01-31 14:22:32 -0700

    Part 04 - 06 - SLF4J Logging Facility

--- onlineshopping/src/main/java/net/ks/onlineshopping/controller/PageController.java
+++ onlineshopping/src/main/java/net/ks/onlineshopping/controller/PageController.java
@@ -2,6 +2,8 @@ package net.ks.onlineshopping.controller;
 
 import javax.enterprise.inject.spi.Producer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +18,8 @@ import net.ks.shoppingbackend.dto.Product;
 @Controller
 public class PageController {
 
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+	
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
@@ -27,6 +31,10 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title","Home");
+		
+		logger.info("Inside PageController index method - INFO");
+		logger.debug("Inside PageController index method - DEBUG");
+		
 		
 		//passing the list of categories
 		mv.addObject("categories", categoryDAO.list());
